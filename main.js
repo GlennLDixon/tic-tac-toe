7d8255ed6722b558adb9d2e7e232d795c357a37f
Glenn Dixon <glenndixon19@gmail.com>
2018-10-20 18:34:06 -0500

    declaring consts for the symbols, final results and questions

---
+++ main.js
@@ -0,0 +1,29 @@
+/* Play Tic Tac Toe, against a friend or the computer.
+If you choose to play against the computer, the 'Computer Player' will use the getBestMove function to choose one of the best possible moves
+ */ 
+
+// -- Constants --
+const SYMBOLS = {
+    x: 'X',
+    o: 'O'
+}
+
+const Result = {
+    incomplete: 0,
+    playerXWon: SYMBOLS.x,
+    playerOWon: SYMBOLS.o,
+    tie: 3
+}
+
+const VIEW = {
+    question1: 1,
+    question2: 2,
+    game: 3,
+    result: 4
+}
+
+
+
+
+
+
